7b10f3cdce643867f409bf4c230e9f188eda57c6
Darby Behnken <dtbehnken67@gmail.com>
2022-03-09 11:56:42 -0700

    submit new destination updated and running

--- index.js
+++ index.js
@@ -48,10 +48,7 @@ function renderDestination(location) {
     likesButton.className = "likes-button"
     likesButton.textContent = "like"
     likesButton.addEventListener("click", function(e) {
-<<<<<<< HEAD
         e.stopPropagation
-=======
->>>>>>> a33fef5c6e2781d7bd8355938ddc4b17b774f4a4
       ++location.likes
       likesNum.textContent = location.likes
       })
